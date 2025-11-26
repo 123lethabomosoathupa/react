bad197232b25848579593a425251cc2c16a2ed1d
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2025-11-26 15:51:38 +0200

    update

--- Classwork/chapter-4/src/Rating.js
+++ Classwork/chapter-4/src/Rating.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 
 class Rating extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = { rating: this.props.rating };
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick(ratingValue){
-        this.setState({rating: ratingValue});
+    handleClick(ratingValue) {
+        this.setState({ rating: ratingValue });
     }
 
     render() {
@@ -17,29 +17,29 @@ class Rating extends Component {
             <div style={styles.starStyle}>
                 <h1>Rating: {this.state.rating}</h1>
                 {this.state.rating >= 1 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 1)}/>
+                    <IoIosStar onClick={this.handleClick.bind(this, 1)} />
                 ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 1)}/>
+                    <IoIosStarOutline onClick={this.handleClick.bind(this, 1)} />
                 )}
                 {this.state.rating >= 2 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 2)}/>
+                    <IoIosStar onClick={this.handleClick.bind(this, 2)} />
                 ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 2)}/>
+                    <IoIosStarOutline onClick={this.handleClick.bind(this, 2)} />
                 )}
                 {this.state.rating >= 3 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 3)}/>
+                    <IoIosStar onClick={this.handleClick.bind(this, 3)} />
                 ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 3)}/>
+                    <IoIosStarOutline onClick={this.handleClick.bind(this, 3)} />
                 )}
                 {this.state.rating >= 4 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 4)}/>
+                    <IoIosStar onClick={this.handleClick.bind(this, 4)} />
                 ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 4)}/>
+                    <IoIosStarOutline onClick={this.handleClick.bind(this, 4)} />
                 )}
                 {this.state.rating >= 5 ? (
-                    <IoIosStar onClick={this.handleClick.bind(this, 5)}/>
+                    <IoIosStar onClick={this.handleClick.bind(this, 5)} />
                 ) : (
-                    <IoIosStarOutline onClick={this.handleClick.bind(this, 5)}/>
+                    <IoIosStarOutline onClick={this.handleClick.bind(this, 5)} />
                 )}
             </div>
         );
