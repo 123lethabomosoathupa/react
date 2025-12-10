3ab1abb4541241be4bf93eb35552c5a35144edcd
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2025-12-10 09:48:32 +0200

    update

---
+++ Project/Tutorial 7/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDMugfPRlJ9-rX4J1xi9hnvcwHKkVr6a14",
+  authDomain: "todolist-fc678shd.firebaseapp.com",
+  databaseURL: "https://todolist-fc678shd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todolist-fc678shd",
+  storageBucket: "todolist-fc678shd.firebasestorage.app",
+  messagingSenderId: "889284919596",
+  appId: "1:889284919596:web:8ba35004553cd8a2882c89"
+};
